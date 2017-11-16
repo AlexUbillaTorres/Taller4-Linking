34624148e54d01a87428678213b1d110c03cb614
AlexUbillaTorres <33100671+AlexUbillaTorres@users.noreply.github.com>
2017-11-15 23:11:55 -0500

    Update multvec.c

--- multvec.c
+++ multvec.c
@@ -1,10 +1,11 @@
 #include "vector.h"
 
-void multvec(vector c, vector d,
-             vector a) 
+vector multvec(vector c, vector d) 
 {
+    vector a;
+    
     a.x=c.x*d.x;
     a.y=c.y*d.y;
-  
-    printf("a = [%d %d]\n", a.x, a.y);
+    
+    return a;
 }
