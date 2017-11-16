f1773f8f4db21a975ad9d313a93132cac1d3dd9a
AlexUbillaTorres <33100671+AlexUbillaTorres@users.noreply.github.com>
2017-11-15 23:11:25 -0500

    Update addvec.c

--- addvec.c
+++ addvec.c
@@ -1,10 +1,11 @@
 #include "vector.h"
 
-void addvec(vector c, vector d,
-	    vector z) 
+vector addvec(vector c, vector d) 
 {
+   vector z;
+   
    z.x=c.x+d.x;
    z.y=c.y+d.y;
-	
-   printf("z = [%d %d]\n", z.x, z.y);
+   
+   return z;
 }
